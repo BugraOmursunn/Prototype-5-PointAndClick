0d259991b5956e90f2a0d382611a078bc9a35cbc
Bugra.Omursun <bugra.omursun@gmail.com>
2021-05-17 14:54:09 +0300

    Source Files

---
+++ PointAndClick/Assets/PointAndClick.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointAndClick : MonoBehaviour
+{
+    [SerializeField] private GameObject Character;
+    private Vector3 InputPos, targetPos;
+    private float mousePosX, mousePosY;
+    [SerializeField] private float moveSpeed;
+    private Vector3 diff;
+    void Update()
+    {
+
+        /* Without Mathf.Clamp
+        if (Input.GetMouseButton(0))
+        {
+            diff = Input.mousePosition - Camera.main.WorldToScreenPoint(Character.transform.position);
+            targetPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Mathf.Abs(diff.z)));
+        }
+        Character.transform.position = Vector3.Lerp(Character.transform.position, targetPos, Time.deltaTime * moveSpeed);
+        */
+
+        if (Input.GetMouseButton(0))
+        {
+            InputPos = Input.mousePosition;
+            diff = InputPos - Camera.main.WorldToScreenPoint(Character.transform.position);
+
+            mousePosX = Mathf.Clamp(InputPos.x, Screen.width / 10, Screen.width - Screen.width / 10);
+            mousePosY = Mathf.Clamp(InputPos.y, Screen.height / 10, Screen.height - Screen.height / 10);
+
+            targetPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePosX, mousePosY, Mathf.Abs(diff.z)));
+        }
+        Character.transform.position = Vector3.Lerp(Character.transform.position, targetPos, Time.deltaTime * moveSpeed);
+    }
+}
---
+++ PointAndClick/Assets/Test.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private int Carpan = 3;
+    private void Start()
+    {
+        Debug.Log(AtisHizi);
+        AtisHizi = 5;
+    }
+    public int AtisHizi
+    {
+
+        get
+        {
+            return Carpan;
+        }
+        set
+        {
+            Carpan = value;
+            test2();
+        }
+    }
+    void test2()
+    {
+        Debug.Log(AtisHizi);
+    }
+}
